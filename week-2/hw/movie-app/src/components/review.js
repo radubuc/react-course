21b023754ee5f520c67390e0a7a9b40e06348ac0
Renee Dubuc <rdubuc@GYKantler.com>
2022-02-26 16:12:32 -0500

    Added if statement with map method to review component

--- week-2/hw/movie-app/src/components/review.js
+++ week-2/hw/movie-app/src/components/review.js
@@ -21,29 +21,34 @@ export default class Review extends React.Component {
         //How do I add reviews to a specific movie?
         //How do I get star rating pulled in? Hardcoding it for now
         //Should I call this array Spongebob Movie-only reviews?
-        let reviews = [
-            {
-                movie: "The Spongebob Squarepants Movie",
-                username: 'ThinningNotBald63',
-                content: "Alec Baldwin has no comment.",
-                rating: "⭐",
-            },
-            {
-                movie: "The Spongebob Squarepants Movie",
-                username: 'TheHoff52',
-                content: "This is David Hasselhoff's review: 'It was great fun and to this day around the world kids stop me and say, 'Are you David Hasselhoff?' because I was the only human in the picture. It's amazing - so many of the kids were so young and didn't see Baywatch and Knight Rider so I got a whole new legion of fans.'",
-                rating: "⭐⭐⭐⭐⭐",
-            },
-            {
-                movie: "The Spongebob Squarepants Movie",
-                username: 'ScarJo1',
-                content: "This is Scarlett Johansson's review: 'The coolest thing about being a mermaid is wearing a seashell bra.'",
-                rating: "⭐⭐⭐⭐⭐",
-            },
+        // let reviews = [
+        //     {
+        //         movie: "The Spongebob Squarepants Movie",
+        //         username: 'ThinningNotBald63',
+        //         content: "Alec Baldwin has no comment.",
+        //         rating: "⭐",
+        //     },
+        //     {
+        //         movie: "The Spongebob Squarepants Movie",
+        //         username: 'TheHoff52',
+        //         content: "This is David Hasselhoff's review: 'It was great fun and to this day around the world kids stop me and say, 'Are you David Hasselhoff?' because I was the only human in the picture. It's amazing - so many of the kids were so young and didn't see Baywatch and Knight Rider so I got a whole new legion of fans.'",
+        //         rating: "⭐⭐⭐⭐⭐",
+        //     },
+        //     {
+        //         movie: "The Spongebob Squarepants Movie",
+        //         username: 'ScarJo1',
+        //         content: "This is Scarlett Johansson's review: 'The coolest thing about being a mermaid is wearing a seashell bra.'",
+        //         rating: "⭐⭐⭐⭐⭐",
+        //     },
             
-        ];
+        // ];
 
         let reviewsArray;
+        if (this.state.reviews) {
+            reviewsArray = this.state.reviews.map((review, index) =>
+            <li key={index}>{review}</li>
+            )
+        }
 
         return (
             <div>
