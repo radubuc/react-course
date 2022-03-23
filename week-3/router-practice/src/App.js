6c07e77c958a8d7d2f03300093e53eb6512cc228
Renee Dubuc <rdubuc@GYKantler.com>
2022-03-22 21:27:23 -0400

    Finished Hooks video notes

--- week-3/router-practice/src/App.js
+++ week-3/router-practice/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+export default function App() {
+  //First const is destructured pair in an array. isOn is value in state (boolean). setIsOn is name of method that will update state property. False is default value as a parameter. When we call setIsOn, we can pass in new useState value.
+
+  //Second const is click counter for button
+  const [isOn, setIsOn] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
+  
+  //Hook is like the class component equivalent of performing an HTTP request in componentDidMount lifecycle method.
+  useEffect(() => {
+	  document.title = isOn ? 'ON' : 'OFF'; //Changes title of browser tab based on evaluation of isOn
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{isOn ? 'Yes, it is on!' : 'OFF'}</p>
+      <p>Times Clicked: {clickCount}</p>
+      <button onClick={() => {
+        	setIsOn(!isOn);
+			setClickCount(clickCount + 1);
+        }
+      }>Switch</button>
     </div>
-  );
+  ); //Markup in <p> says "If isOn is true, output 'Yes, it is on!'. If it's false, output 'OFF'." When button is clicked, it takes opposite of isOn and updates value. Times Clicked markup dynamically pulls in how many times was clicked and displays. 
 }
 
-export default App;
+
