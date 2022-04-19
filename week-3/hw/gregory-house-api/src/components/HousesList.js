86569a0de2363a050a11a3cd00030060e17fd2d5
Renee Dubuc <rdubuc@GYKantler.com>
2022-04-18 21:04:36 -0400

    Houses List fetch and updateHouse methods

---
+++ week-3/hw/gregory-house-api/src/components/HousesList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { House } from './House.js';
+import { housesApi } from '../rest/HousesApi.js';
+
+export class HousesList extends React.Component {
+    state = {
+        houses: []
+    };
+
+    componentDidMount() {
+        this.fetchHouses();
+    };
+
+    //Get all houses
+    fetchHouses = async () => {
+        const houses = await housesApi.get(); //Assign houses the response from await
+        this.setState({ houses });
+    };
+
+    updateHouse = async (updatedHouse) => { 
+        await housesApi.put(updatedHouse); 
+        this.fetchHouses(); //Once response comes back, call fetchHouses() method, which will run and then set state = to all houses again with update included
+    };
+
+    render() {
+        return (
+            <div className='house-list'> 
+                {this.state.houses.map((house) => (
+                    <House 
+                        house={house}
+                        key={house._id}
+                        updateHouse={this.updateHouse}
+                    />
+                ))}
+            </div>
+        ) 
+        //Lines 28-33: Map each house from our state's houses. With each instance of House, house name and ID (passed in as props) are being mapped. Then pass in updateHouse() method, so I can pass down from a child
+    }
+}
+
