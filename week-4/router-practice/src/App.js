3d8d7a43706ccdb8137a9044103c120e8f4b0570
Renee Dubuc <rdubuc@GYKantler.com>
2022-09-02 21:04:12 -0400

    Tried downgrading to react 17.0.2 and react dom 17.0.2 and react router dom 5.3.0

--- week-4/router-practice/src/App.js
+++ week-4/router-practice/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch
+} from 'react-router-dom';
 
-function App() {
+export default function App() {
+  const posts = [
+    {
+      id: 1,
+      title: 'My First Post',
+      date: '4-7-2020',
+      content: 'This is my first post!'
+    },
+    {
+      id: 2,
+      title: 'Checking In',
+      date: '4-8-2020',
+      content: 'This week sucked.'
+    },
+    {
+      id: 3,
+      title: 'Vacation Time',
+      date: '4-9-2020',
+      content: 'TGIF'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/posts">
+            <Posts posts={posts} />
+          </Route>
+          <Route path="/friends">
+            <Friends names={["Chantel", "Ricky", "Sabeen"]} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Friends() {
+  return <h2>Friends</h2>
+}
+
+function Posts() {
+  return <h2>Posts</h2>
+}
