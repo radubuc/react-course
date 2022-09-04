aba4f92206105e1b5c055f0d96a453f27ddbce6f
Renee Dubuc <rdubuc@GYKantler.com>
2022-09-04 14:58:23 -0400

    Made router-practice-3, which combines the videos instructions w/ Matt's fixes!

--- week-4/router-practice-2/src/components/test.js
+++ week-4/router-practice-2/src/components/test.js
@@ -1,9 +1,16 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
-import ContactUs from './ContactUs';
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom';
+// import ContactUs from './ContactUs';
  
 class NavBar extends React.Component{
-
     render(){
         return(
             <Router>
@@ -78,10 +85,66 @@ function Child() {
     return <h2>Home</h2>
   }
   
-  function Friends() {
-    return <h2><ContactUs /></h2>
+  function Friends(props) {
+    const {names} = props;
+
+    return (
+      <div>
+        <ul>
+          {names.map((friend, index) => (
+            <li key={index}>{friend}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
   
-  function Posts() {
-    return <h2>Posts</h2>
+  //POSTS COMPONENT
+  function Posts({posts}) {
+    const match = useRouteMatch();
+    const findPostById = (id) => 
+      posts.filter((post) => post.id == id)[0];
+
+    return (
+      <div>
+        <h2>Posts</h2>
+        <ul>
+          {posts.map((post, index) => {
+            return (
+              <li key={index}>
+                <Link to={`${match.url}/${post.id}`}>
+                  {post.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <Switch>
+          <Route
+            path={`${match.path}/:postId`}
+            render={(props) => (
+              <Post
+                {...props}
+                data={findPostById(props.match.params.postId)}
+              />
+            )}
+          />
+          <Route path={match.path}>
+            <h3>Please select a post</h3>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+
+  //POST COMPONENT
+  function Post(props) {
+    const {data} = props;
+    return (
+      <div>
+        <h3>{data.title}</h3>
+        <h4>{data.date}</h4>
+        <p>{data.content}</p>
+      </div>
+    )
   }
