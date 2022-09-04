bebd2bddb65d560a5a3a1f50bbf0bc5e08752396
Renee Dubuc <rdubuc@GYKantler.com>
2022-09-04 15:12:47 -0400

    Resynced mostly-working-react-17-basic-app-09032022-220pm

---
+++ code-library/mostly-working-react-17-basic-app-09032022-220pm/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './components/test';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default App;
